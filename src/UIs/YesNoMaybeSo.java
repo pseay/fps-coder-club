720b680c2d8b97ed39bce98fbcbdf8e59d30dcd2
Preston Seay <preston.seay@outlook.com>
2021-01-14 16:31:10 -0800

    added JOptionPane example

---
+++ src/UIs/YesNoMaybeSo.java
@@ -0,0 +1,14 @@
+package UIs;
+
+import javax.swing.*;
+
+public class YesNoMaybeSo {
+    public static void main(String[] args) {
+        String response = JOptionPane.showInputDialog("Is chocolate better than vanilla?");
+        if (response.toLowerCase().equals("yes")) {
+            JOptionPane.showMessageDialog(null, "You know the truth.");
+        } else if (response.toLowerCase().equals("no")) {
+            JOptionPane.showMessageDialog(null, "WRONG! Chocolate is better!");
+        }
+    }
+}
